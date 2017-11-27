94b0e6378b5529e9536eb376c4310395d88f9630
joonsson <joonsson@gmail.com>
2017-11-27 11:31:14 +0100

    Added stars to set rating and made them interactive

--- code.js
+++ code.js
@@ -1,8 +1,47 @@
-function renderMovie(movie){
+let rating = 5;
+function renderMovie(movie) {
     document.getElementById("title").innerText = movie.title;
     document.getElementById("image").src = movie.image;
     document.getElementById("text").innerText = movie.text;
     document.getElementById("list").innerHTML = movie.actors;
-  }
+}
 
-  renderMovie(movieData);
+function changeStarRating(grade) {
+    let stars = document.getElementById("starDiv");
+    stars.innerHTML="";
+    for (let i = 1; i <= grade; i++) {
+        let filledStar = document.createElement("span");
+        filledStar.innerHTML="&#9733";
+        filledStar.className = "filled star";
+        filledStar.addEventListener("click", function() {
+            rating = i;
+            changeStarRating(i);
+        });
+        filledStar.addEventListener("mouseenter", function() {
+            changeStarRating(i);
+        });
+        filledStar.addEventListener("mouseleave", function() {
+            changeStarRating(rating);
+        });
+        stars.appendChild(filledStar);
+    }
+    for (let i = grade + 1; i <= 5; i++) {
+        let emptyStar = document.createElement("span");
+        emptyStar.innerHTML="&#9733";
+        emptyStar.className = "empty star";
+        emptyStar.addEventListener("click", function() {
+            rating = i;
+            changeStarRating(i);
+        });
+        emptyStar.addEventListener("mouseenter", function() {
+            changeStarRating(i);
+        });
+        emptyStar.addEventListener("mouseleave", function() {
+            changeStarRating(rating);
+        });
+        stars.appendChild(emptyStar);
+    }
+}
+
+renderMovie(movieData);
+changeStarRating(rating);
