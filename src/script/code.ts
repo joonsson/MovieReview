d98700ecc1218a64deff539cb819d018a13955b2
joonsson <joonsson@gmail.com>
2017-11-29 15:27:09 +0100

    Broke everything

--- src/script/code.ts
+++ src/script/code.ts
@@ -5,36 +5,48 @@ import { SWep6 } from "./data";
 import { Ireview } from "./interface";
 // let audio = new Audio("StarWars.mp3");
 const app = angular.module("reviewApp", []);
-let rating = SWep4.rating;
+let fourRating = SWep4.rating;
+let fiveRating = SWep5.rating;
+let sixRating = SWep6.rating;
+let rating = fourRating;
 let movie = 4;
 let timeout;
 
-app.controller("reviewController", ($scope) => {
-    $scope.movie = SWep4;
-    $scope.rating = rating;
+app.controller("reviewController", () => {
+    this.movie = SWep4;
+    this.rating = fourRating;
 
-    $scope.changeMovie = () => {
+    this.changeMovie = () => {
         if (movie === 4) {
             movie++;
-            $scope.movie = SWep5;
+            fourRating = rating;
+            this.movie = SWep5;
+            rating = fiveRating;
+            this.changeRating(rating);
         } else if (movie === 5) {
             movie++;
-            $scope.movie = SWep6;
+            fiveRating = rating;
+            this.movie = SWep6;
+            rating = sixRating;
+            this.changeRating(rating);
         } else {
             movie = 4;
-            $scope.movie = SWep4;
+            sixRating = rating;
+            this.movie = SWep4;
+            rating = fourRating;
+            this.changeRating(rating);
         }
     };
 
-    $scope.changeRating = (newRating) => {
+    this.changeRating = (newRating) => {
         rating = newRating;
-        $scope.rating = rating;
+        this.rating = rating;
     };
-    $scope.changeStarRating = (newRating) => {
+    this.changeStarRating = (newRating) => {
         if (newRating === 0) {
-            $scope.rating = rating;
+            this.rating = rating;
         } else {
-            $scope.rating = newRating;
+            this.rating = newRating;
         }
     };
 });
@@ -54,50 +66,8 @@ function restart() {
         crawl.classList.add("crawl");
     }, 1);
 
-    /* const crawl = document.getElementById("textDiv");
-    const newCrawl = crawl.cloneNode(true);
-    crawl.insertBefore(newCrawl, crawl);
-
-    document.querySelector("." + crawl.getAttribute("class") + ":last").remove(); */
     delayedRestart();
 }
-
-/* ui.stars.click("span", (e) => {
-    const star = $(e.target);
-    const starri = parseInt(star.attr("id"), 10);
-    if (starri <= 6 && starri >= 1) {
-        rating = starri;
-        changeStarRating(starri);
-    }
-});
-ui.stars.mouseover("span", (e) => {
-    const star = $(e.target);
-    const starri = parseInt(star.attr("id"), 10);
-    if (starri <= 6 && starri >= 1) {
-        changeStarRating(starri);
-    }
-});
-ui.stars.mouseleave("span", () => {
-    changeStarRating(rating);
-});
-
-function renderMovie(movie: Ireview) {
-    ui.title.attr("src", movie.title);
-    ui.image.attr("src", movie.image);
-    ui.text.text(movie.text);
-    ui.list.html(movie.actors);
-}
-
-function changeStarRating(grade) {
-    for (let i = 1; i <= 5; i++) {
-        const star = $("#" + i);
-        if (i <= grade) {
-            star.addClass("filled");
-        } else {
-            star.removeClass("filled");
-        }
-    }
-} */
 
 window.onload = () => {
     // audio.play();
